177036bc1bfd3ac016fb9dc17fe4b958b624f08a
Mike Chlipala <mchlipala@gmail.com>
2012-07-13 19:21:03 -0400

    create 15 tracks

--- test/factories/playlists.rb
+++ test/factories/playlists.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
 
   factory :playlist_with_tracks, parent: :playlist do
     after(:create) do |playlist, evaluator|
-      10.times do
+      15.times do
         track = FactoryGirl.build(:track)
         playlist.tracks << track
       end
