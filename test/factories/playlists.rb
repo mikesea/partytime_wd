c8a4bd1a8da7212d55d2b7d68c0705da1d871b63
Mike Chlipala <mchlipala@gmail.com>
2012-07-13 19:21:51 -0400

    remove factory for playlist with many tracks

--- test/factories/playlists.rb
+++ test/factories/playlists.rb
@@ -10,14 +10,4 @@ FactoryGirl.define do
       playlist.save
     end
   end
-
-  factory :playlist_with_many_tracks, parent: :playlist do
-    after(:create) do |playlist, evaluator|
-      50.times do
-        track = FactoryGirl.build(:track)
-        playlist.tracks << track
-      end
-      playlist.save
-    end
-  end
 end
