d21ee5313bbeb4c7aea173f9033b8378894a523f
Mike Chlipala <mchlipala@gmail.com>
2012-07-13 15:32:15 -0400

    add album_title to track factory

--- test/factories/tracks.rb
+++ test/factories/tracks.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :track do
+    sequence(:album_title) { |n| "album_#{n}" }
     sequence(:artist_name) { |n| "artist_#{n}" }
     sequence(:title) { |n| "title_#{n}" }
     sequence(:rdio_id) { |n| "#{n}" }
