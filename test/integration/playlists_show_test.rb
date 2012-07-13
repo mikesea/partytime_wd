7b92ee877982c5461055f7a376fe9fe4a6b56eaf
Mike Chlipala <mchlipala@gmail.com>
2012-07-13 14:19:28 -0400

    cleanup the db after each test

--- test/integration/playlists_show_test.rb
+++ test/integration/playlists_show_test.rb
@@ -1,7 +1,18 @@
 require 'test_helpers'
- class PlaylistsShowSpec < MiniTest::Spec
+
+class PlaylistsShowSpec < MiniTest::Spec
+  before :each do
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  let(:playlist) { FactoryGirl.create(:playlist) }
+
   it "performs request" do
-    TestApi.get "/playlists/:id", {}
+    TestApi.get "/playlists/:id", id: playlist.id
     assert_api_response
   end
 end
