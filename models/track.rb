db8f054ce4c8440d96c4c39518066aa4353d2135
Mike Chlipala <mchlipala@gmail.com>
2012-07-13 20:50:53 -0400

    return track id in response

--- models/track.rb
+++ models/track.rb
@@ -2,7 +2,9 @@ class Track < ActiveRecord::Base
   belongs_to :playlist
 
   def as_json(*options)
-    { title: title,
+    { 
+      id: id,
+      title: title,
       album_title: album_title,
       artist_name: artist_name,
       rdio_id: rdio_id }
