32cd3874642f7a7421914d73231c2bd7f89ab98c
Mike Chlipala <mchlipala@gmail.com>
2012-07-13 20:29:50 -0400

    test for track deletion on a different playlist

--- test/integration/playlist_tracks_destroy_test.rb
+++ test/integration/playlist_tracks_destroy_test.rb
@@ -10,6 +10,7 @@ class PlaylistsTracksDeleteSpec < MiniTest::Spec
   end
 
   let(:playlist) { FactoryGirl.create(:playlist_with_tracks) }
+  let(:playlist2) { FactoryGirl.create(:playlist_with_tracks) }
 
   it "performs request" do
     TestApi.delete "/playlists/:playlist_id/tracks/:id", { playlist_id: playlist.id, id: playlist.tracks.last.id }
@@ -22,4 +23,9 @@ class PlaylistsTracksDeleteSpec < MiniTest::Spec
     refute playlist.tracks.include?(track)
   end
 
+  it "does not remove a track from a different playlist" do
+    track = playlist2.tracks.last
+    TestApi.delete "/playlists/:playlist_id/tracks/:id", { playlist_id: playlist.id, id: track.id }
+    assert_equal 400, TestApi.json_response.status
+  end
 end
