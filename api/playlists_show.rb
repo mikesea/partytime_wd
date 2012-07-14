1c706823d02df3f783276d1e82352351c8e7f082
Mike Chlipala <mchlipala@gmail.com>
2012-07-14 11:23:41 -0400

    documentation for playlists#show

--- api/playlists_show.rb
+++ api/playlists_show.rb
@@ -4,17 +4,20 @@ describe_service "playlists/:id" do |service|
   service.disable_auth # on by default
   
   # INPUT
-  service.param.integer :id
+  service.param.integer :id,
+                        doc: "The ID of the playlist to which the track belongs.",
+                        null: false,
+                        required: true
   
   # OUTPUT
   service.response do |response|
     response.object do |playlist|
-      playlist.integer :id
+      playlist.integer :id, doc: "The ID of the playlist that was fetched."
       playlist.array :tracks do |track|
-        track.string :title
-        track.string :album_title
-        track.string :artist_name
-        track.string :rdio_id
+        track.string :title, doc: "The title of the track."
+        track.string :album_title, doc: "The title of the album to which the track belongs."
+        track.string :artist_name, doc: "The name of the track's artist."
+        track.string :rdio_id, doc: "The Rdio ID of the track."
       end
     end
   end
@@ -22,7 +25,8 @@ describe_service "playlists/:id" do |service|
   # DOCUMENTATION
   service.documentation do |doc|
     doc.overall "This service returns a playlist."
-    doc.example "<code>curl 'http://localhost:9292/playlists/123'</code>"
+    doc.param :id, "The ID of the playlist being fetched (e.g., 123)."
+    doc.example "<code>curl 'http://localhost:9292/playlists/1'</code>"
   end
   
   # ACTION/IMPLEMENTATION
