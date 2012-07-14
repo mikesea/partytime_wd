4d8121ce0a80c3c6d12bcda256436e5662bfc340
Mike Chlipala <mchlipala@gmail.com>
2012-07-14 11:23:57 -0400

    documentation for playlists#create

--- api/playlists_create.rb
+++ api/playlists_create.rb
@@ -4,29 +4,35 @@ describe_service "playlists" do |service|
   service.disable_auth # on by default
   
   # INPUT
-  service.params do |p|
+  service.param do |p|
     p.array :artists do |artist|
       artist.string :artist_name,
-                    :doc => "The name of an artist used to recommend tracks for the playlist"
+                    doc: "The name of an artist used to recommend tracks for the playlist",
+                    required: true,
+                    null: false
     end
   end
   
   # OUTPUT
   service.response do |response|
     response.object do |playlist|
-      playlist.integer :id
+      playlist.integer :id, doc: "The ID of the track that has been deleted."
       playlist.array :tracks do |track|
-        track.string :title
-        track.string :album_title
-        track.string :artist_name
-        track.string :rdio_id
+        track.string :title, doc: "The title of the track."
+        track.string :album_title, doc: "The title of the album to which the track belongs."
+        track.string :artist_name, doc: "The name of the track's artist."
+        track.string :rdio_id, doc: "The Rdio ID of the track."
       end
     end
   end
   
   # DOCUMENTATION
   service.documentation do |doc|
-    doc.overall "This service generates a 15-song playlist, taking up to 5 artists to recommend tracks from."
+    doc.overall <<-DOC
+    This service accepts a list of musical <code>artists</code>,
+    and then builds 15 track playlist with similarities to the provided <code>artists</code>.
+    DOC
+    doc.param :artists, "An list artists to use for track recommendations."
     doc.example "<code>curl -d 'artists=Weezer, Black Keys', http://localhost:9292/playlists</code>"
   end
   
