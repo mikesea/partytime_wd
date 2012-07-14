2e18cea357b76b79ef8cad9890c383ef71670886
Mike Chlipala <mchlipala@gmail.com>
2012-07-14 11:24:20 -0400

    documentation for playlist_tracks#create

--- api/playlist_tracks_create.rb
+++ api/playlist_tracks_create.rb
@@ -5,30 +5,44 @@ describe_service "playlists/:playlist_id/tracks" do |service|
   
   # INPUT
   service.params do |p|
-    p.integer :playlist_id
+    p.integer :playlist_id,
+              doc: "The ID of the playlist to which the track belongs.",
+              null: false,
+              required: true
     p.namespace :track do |track|
-      track.string :title
-      track.string :album_title
-      track.string :artist_name
-      track.string :rdio_id
+      track.string :title,
+                    doc: "The title of the track.",
+                    required: true, null: false
+      track.string :album_title,
+                    doc: "The title of the album to which the track belongs.",
+                    required: true, null: false
+      track.string :artist_name,
+                    doc: "The name of the track's artist.",
+                    required: true, null: false
+      track.string :rdio_id,
+                    doc: "The Rdio ID of the track.",
+                    required: true, null: false
     end
   end
   
   # OUTPUT
   service.response do |response|
     response.object do |track|
-      track.string :title
-      track.string :album_title
-      track.string :artist_name
-      track.string :rdio_id
+        track.string :title, doc: "The title of the track."
+        track.string :album_title, doc: "The title of the album to which the track belongs."
+        track.string :artist_name, doc: "The name of the track's artist."
+        track.string :rdio_id, doc: "The Rdio ID of the track."
     end
   end
   
   # DOCUMENTATION
   service.documentation do |doc|
     doc.overall "This service adds a track to an existing playlist."
-    doc.example "<code>
-      curl -d 'track[title]=song1&track[album_title]=album1&track[artist_name]=artist1&track[rdio_id]=t123' http://localhost:9292/playlists/1/tracks</code>"
+    doc.example <<-DOC
+    <code>
+      curl -d 'track[title]=song1&track[album_title]=album1&track[artist_name]=artist1&track[rdio_id]=t123' http://localhost:9292/playlists/1/tracks
+    </code>
+    DOC
   end
   
   # ACTION/IMPLEMENTATION
